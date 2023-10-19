48444622bd647461594ba79f1db5d694d5ccb573
hakimfrh <hakimfrh145@gmail.com>
2023-10-19 11:47:39 +0700

    upload

--- simpletest.php
+++ simpletest.php
@@ -1,14 +1,16 @@
 <?php
-    use PHPUnit\Framework\TestCase;
-    require_once "wordcount.php";
+    use PHPUnit\Framework\TestCase;//Menggunakan PHPUnit
+    require_once "wordcount.php";//membutuhkan wordcount.php
     class Simpletest extends TestCase {
         public function testcountword (){
+            // Menggunakan Class yang akan di test / mendeklarasi class
             $wc = new wordcount();
-
-            $TestSentence = "My name is joko";
-            $Wordcount = $wc ->countword($TestSentence);
             
-            $this -> assertEquals(4, $Wordcount);
+            //Masukkan kata kata 
+            $TestSentence = "Aku lapar ingin makan";// total kata = 4
+            $Wordcount = $wc ->countword($TestSentence);//Menghitung kata dari variable katatesting
+            
+            $this -> assertEquals(4, $Wordcount);// jika kata == 4, Berarti Benar
         }
     }
 ?>
--- wordcount.php
+++ wordcount.php
@@ -1,7 +1,7 @@
 <?php
     class wordcount{
-        public function countword ($sentence){
-            return count(explode(" ", $sentence));
+        public function countword ($sentence){//membuat fungsi untuk menampung kata
+            return count(explode(" ", $sentence));//memecah kata menggunakan exploda agar bisa dihiung
         }
     } 
 ?>
